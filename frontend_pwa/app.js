e4f4145d56595944486bd6c2b1fa82e8b13e18de
IsaacRB32 <148009185+IsaacRB32@users.noreply.github.com>
2026-05-16 20:57:29 -0600

    Fix: Ajuste de rutas absolutas de producción para la API

--- frontend_pwa/app.js
+++ frontend_pwa/app.js
@@ -1,5 +1,5 @@
 /* frontend_pwa/app.js */
-const API_URL = "https://crytographyproyectecdsa.onrender.com";
+const API_URL = "https://crytographyproyectecdsa.onrender.com/api/v1";
 
 // VARIABLES DE ESTADO LOCAL (Seguridad en memoria RAM)
 let usuarioSesion = null;      // Datos del cliente logeado
